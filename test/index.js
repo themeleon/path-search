61aac9510af74d8d530510d2020cbbb1d40c5a8a
Valérian Galliat <val@codejam.info>
2014-12-14 20:42:04 +0100

    Fix test name case

--- test/index.js
+++ test/index.js
@@ -7,19 +7,19 @@ var s = require('..');
 process.chdir(__dirname);
 
 describe('#pathSearch', function () {
-  it('Should work with a single directory', function () {
+  it('should work with a single directory', function () {
     e(r('a/a'), s(['a'], 'a'));
   });
 
-  it('Should take the first found file from path array', function () {
+  it('should take the first found file from path array', function () {
     e(r('b/a'), s(['b', 'a'], 'a'));
   });
 
-  it('Should walk up the path until a file is found', function () {
+  it('should walk up the path until a file is found', function () {
     e(r('b/b'), s(['a', 'b'], 'b'));
   });
 
-  it('Should return `undefined` when no file is found', function () {
+  it('should return `undefined` when no file is found', function () {
     e(undefined, s([]));
     e(undefined, s(['a', 'b'], 'c'));
   });
