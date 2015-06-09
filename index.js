460d6a868120702181d7995b5396c97a735a3062
Valérian Galliat <val@codejam.info>
2015-06-09 14:03:55 +0200

    Move to JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-'use strict';
-
-var nodePath = require('path');
-var fs = require('fs');
+var nodePath = require('path')
+var fs = require('fs')
 
 /**
  * @param {Array} path
  * @param {String} name
  */
-module.exports = function pathSearch(path, name) {
+module.exports = function pathSearch (path, name) {
   for (var i = 0; i < path.length; i++) {
-    var candidate = nodePath.resolve(path[i], name);
+    var candidate = nodePath.resolve(path[i], name)
 
     if (fs.existsSync(candidate)) {
-      return candidate;
+      return candidate
     }
   }
-};
+}
